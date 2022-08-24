c00a3975cdd598d71077674a7bb8f7e5966d1473
Nick Parisik <31113934+nparisik@users.noreply.github.com>
2022-08-23 23:08:01 -0400

    Add existing project files

---
+++ src/test/java/com/np/monkeypoxtracker/MonkeypoxtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np.monkeypoxtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeypoxtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
